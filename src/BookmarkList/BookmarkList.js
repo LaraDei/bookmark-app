ecece5640251471a55355cf42531bc9cad837dcf
Lara Samuelson <lara.samuelson@gmail.com>
2020-11-05 08:54:15 -0800

    unpdating context

--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import BookmarksContext from '../BookmarksContext';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
 import './BookmarkList.css'
 
 class BookmarkList extends Component {
-  static defaultProps = {
-    bookmarks: []
-  };
-
+  /* With the context imported, we can attach it to our class component. 
+  We can assign our context to a static property called contextType, 
+  this will give us a new instance property this.context
+  */
+  static contextType = BookmarksContext;
+ 
   render() {
-    const { bookmarks } = this.props
+    
+    const { bookmarks } = this.context
+    console.log({bookmarks})
+    console.log(this)
     return (
       <section className='BookmarkList'>
         <h2>Your bookmarks</h2>
